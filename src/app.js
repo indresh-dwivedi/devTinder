6bc6a9f4a63ec4efefdab06dea5676bc2791259e
indresh-007 <indresh209721@gmail.com>
2026-07-06 11:27:17 +0530

    Created a express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Namaste Indresh Dwivedi!")
+})
+
+app.use("/hello", (req, res) => {
+    res.send("Hello Hello Hello!")
+})
+
+app.use("/test", (req, res) => {
+    res.send("Hello from the server!")
+})
+
+app.listen(7777, () => {
+    console.log("server is successfully listening on port 7777....")
+});
