f26032c54dd0f77b61db81e06b9896b02a393ae7
indresh-007 <indresh209721@gmail.com>
2026-07-25 22:59:55 +0530

    Queries & indexes in DB & Create send connectionRequest API

--- src/models/user.js
+++ src/models/user.js
@@ -40,11 +40,17 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
+        enum: {
+            values: ["male", "female", "others"],
+            message: `{VALUE} is not a valid gender type`,
+        },
+        /*
         validate(value) {
             if(!["male", "female", "others"].includes(value)) {
                 throw new Error("Gender data is not valid");
             }
         },
+        */
     },
     photoUrl: {
         type: String,
@@ -65,7 +71,6 @@ const userSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
-
 
 userSchema.methods.getJWT = async function () {
     const user = this;
@@ -90,4 +95,3 @@ userSchema.methods.validatePassword = async function (passwordInputByUser) {
 };
 
 module.exports = mongoose.model("User", userSchema);
-
