5fb16639fb08e5cac8a4a5efdc1bf5f044c92581
indresh-007 <indresh209721@gmail.com>
2026-08-08 20:42:51 +0530

    Changes to support frontend

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,10 +3,9 @@ const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
     try {
-        // Read the token from the req cookies
         const {token} = req.cookies;
         if (!token) {
-            throw new Error("Token is not valid!");
+            return res.status(401).send("Please Login!");
         }
     
         const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
